146cac8878a0accdf86a233545e4469a51fb0383
Angel Naut <nautangel58@gmail.com>
2025-04-10 15:16:51 -0400

    creacion del repositorio del proyecto final RedNeuronal y subida de archivos

---
+++ RedNeuronal_ProyectoFinal/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RedNeuronal_ProyectoFinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
